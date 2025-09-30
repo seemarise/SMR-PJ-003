3e926ef544c2e49db087a2a1215dead21de0ec43
Abhay Pratap Singh Chauhan <apscprof@gmail.com>
2025-09-30 21:58:54 +0530

    Merge Fixes

--- app/layout.js
+++ app/layout.js
@@ -10,16 +10,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-// <<<<<<< DJ_dev
-      <body className="min-h-screen flex flex-col">
         <body className="min-h-screen flex flex-col">
           <LayoutWrapper>{children}</LayoutWrapper>
         </body>
-//   =======
-//       <body className="bg-gray-50 text-gray-900">
-        {children}
-// >>>>>>> classroom-module
-      </body>
     </html>
   );
 }
