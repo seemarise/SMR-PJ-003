5ce71f992437b244564360141342a6436023f4b0
Himanshu Singh <himanshusinghjbs_cse20@its.edu.in>
2025-10-11 21:06:10 +0530

    Attendence Page

--- app/student/classroom/studentdashboard/attendance/page.js
+++ app/student/classroom/studentdashboard/attendance/page.js
@@ -79,7 +79,7 @@ export default function AttendanceCalendar() {
 
     return (
         <div className="flex min-h-screen flex-col bg-white md:bg-gray-50">
-            <main className="flex-1 px-4 py-6 md:px-8 md:py-10">
+            <main className="flex-1 px-4 py-2 md:px-8">
                 <div className="md:max-w-5xl md:mx-auto">
                     <div className="relative flex items-center justify-between mb-6">
                         <Link href="/student/classroom/studentdashboard" className="p-2 rounded-full bg-blue-100 hover:bg-blue-200 transition md:p-3 md:shadow-sm">
